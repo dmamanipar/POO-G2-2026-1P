c9e43c765dc0845072126afcf8dfb0862fcc2219
renzoanccota <lorengumpgg@gmail.com>
2026-03-18 15:07:42 -0500

    mi primer cambio

---
+++ EjemploG2/src/RenzoJ202312710.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class RenzoJ202312710 {
+    public static void main(String[] arg){
+       ArrayList<String> lista=new ArrayList<>();
+        lista.add("rafael");
+        lista.add("roy");
+        lista.add("ismael");
+        for (String v:lista) {
+            syste
+        }
+    }
+}
