f0ae679f02c2e68ebf46048ab311944af7cc1f94
mayumiba26-jpg <mayumiba26@gmail.com>
2026-03-18 15:16:27 -0500

    mi primer cambio

---
+++ EjemploG2/src/mayumi20252065.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class mayumi20252065 {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("rafael");
+        lista.add("david");
+        lista.add("elias");
+        for(String v:lista) {
+            System.out.println(v);
+
+        }
+
+    }
+
+}
