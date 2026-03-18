7e25bf89326d072bc087c89a0aa0ff4c51ad1138
hernan123654 <hernan123654walter@gmail.com>
2026-03-18 15:17:21 -0500

    mi primer cambio

---
+++ EjemploG2/src/W202514040.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class W202514040 {
+    public static void main(String[] args){
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("yo");
+        lista.add("Ella");
+        lista.add("El");
+
+        for (String v:lista){
+            System.out.println(v);
+
+        }
+    }
+}
