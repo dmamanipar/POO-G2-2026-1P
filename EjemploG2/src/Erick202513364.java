d00583e90d80022bf2fd07c76d5be92b7560e0c0
Erick2222-cpu <ccosisucapucaanthony@gmail.com>
2026-03-18 15:07:01 -0500

    mi primer cambio

---
+++ EjemploG2/src/Erick202513364.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Erick202513364 {
+    public static void main(String [] arg) {
+        ArrayList<String> lista = new ArrayList();
+        lista.add("Erick");
+        lista.add("David");
+        lista.add("Elias");
+
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+}
