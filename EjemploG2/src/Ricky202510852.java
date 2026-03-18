da2189bc423e448ef7e78d5672cc36fe2d9823dd
rickyjherson-ui <rickjherso@gmail.com>
2026-03-18 15:16:09 -0500

    mi primer cambio

---
+++ EjemploG2/src/Ricky202510852.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Ricky202510852 {
+    public static void main(String[] args) {
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("xavi");
+        lista.add("jherson");
+        lista.add("anderson");
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+
+}
