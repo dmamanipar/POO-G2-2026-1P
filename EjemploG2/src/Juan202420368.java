1e1fc0dca59e3c5dc1aca7ddbc48502dfb23b27d
JuanBenique <juanbenique170@gmail.com>
2026-03-18 16:57:16 -0500

    repaso2

--- EjemploG2/src/Juan202420368.java
+++ EjemploG2/src/Juan202420368.java
@@ -1,13 +1,16 @@
+import modelo.Persona;
+
 import java.util.ArrayList;
 
 public class Juan202420368 {
 
     public static void main(String[] args) {
-        ArrayList<String> lista=new ArrayList();
-        lista.add("s");
-        lista.add("s");
-        lista.add("c");
-
+        Persona[] p=new Persona[3];
+        ArrayList<Persona> lista=new ArrayList();
+        lista.add(new Persona("123456", "juan","Benique");
+        lista.add(new Persona("1244456", "juan","Benique");
+        lista.add(new Persona("1234446", "juan","Benique");
+        lista.add(new Persona("123456", "juan","Benique");
         for (String v:lista){
             System.out.println(v);
         }
---
+++ EjemploG2/src/Repaso.java
@@ -0,0 +1,29 @@
+import modelo.Persona;
+
+public class Repaso {
+
+    public static void main (String [] ar ){
+
+       byte a=2;
+
+       short b=456;
+        char c=5675;
+        int d=55445323;
+        long e=5454545444454545454L;
+        float f=1454545.4f;
+        double g=5454545.222;
+        boolean h=true;
+        Persona p=new Persona("43564345","juan","mamani");
+        String valor="Repaso";
+        Persona[] v={};
+         int[][] m={{},{}};
+
+
+
+
+        public static int suma(){
+             return a+b;
+        }
+
+    }
+}
