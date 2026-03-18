2490354c7c268c3c827f66276495e6d042ef862f
BRAULIO-1423 <jhonax.23tn@gmail.com>
2026-03-18 15:16:15 -0500

    mi primer cambio

---
+++ EjemploG2/src/braulio202520641.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class braulio202520641 {
+    public static void main(String[] args) {
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("BIRGILIO");
+        lista.add("BEATRIZ");
+        lista.add("BRAULIO");
+
+        for (String v:lista){
+        System.out.println(v);
+        }
+
+    }
+}
