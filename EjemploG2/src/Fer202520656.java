551654aec602e4b836ce0f0864f91fc5b8b6551a
xJosepi <joshepapaza6@gmail.com>
2026-03-18 15:12:00 -0500

    mi primer cambio

---
+++ EjemploG2/src/Fer202520656.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Fer202520656 {
+
+    public static void main(String[] args){
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Fernando");
+        lista.add("Maryori");
+        lista.add("Asunta");
+
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+}
