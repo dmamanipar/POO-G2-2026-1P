829716ad9b3ff7e079e390ac3e94980a08b94543
ruben7405 <rubenxtlv075@gmail.com>
2026-03-18 15:14:51 -0500

    mi primer trabajohh

---
+++ EjemploG2/src/RUBEN202514032.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RUBEN202514032 {
+    public static void main(String[] arg){
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("lizbeth");
+        lista.add("David");
+        lista.add("Ruben");
+
+        for (String v:lista) {
+            System.out.println(v);
+
+        }
+    }
+
+}
