06bac1f3d9455ef3c358d4d8dad3052b785612b0
SimonRojas566 <romuloyerbarojas@gmail.com>
2026-03-18 15:14:55 -0500

    mi primer cambio

---
+++ EjemploG2/src/romulo202514044.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class romulo202514044 {
+    public static void main(String[] args) {
+
+        ArrayList<String> Lista=new ArrayList<>();
+        Lista.add("Marco");
+        Lista.add("Celia");
+        Lista.add("Beatriz");
+
+
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+}
