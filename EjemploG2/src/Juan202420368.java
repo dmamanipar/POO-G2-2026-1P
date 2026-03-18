1ff55c14d5b18513f964bfbcf7f684e92196c88e
JuanBenique <juanbenique170@gmail.com>
2026-03-18 15:18:05 -0500

    mi primer cambioo

---
+++ EjemploG2/src/Juan202420368.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Juan202420368 {
+
+    public static void main(String[] args) {
+        ArrayList<String> lista=new ArrayList();
+        lista.add("s");
+        lista.add("s");
+        lista.add("c");
+
+        for (String v:lista){
+            System.out.println(v);
+        }
+    }
+}
