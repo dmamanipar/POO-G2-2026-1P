dbcf102b47de8e458b5aa7b5cdcae1338bba0183
Williams-ai <deyvisvargas011@gmail.com>
2026-03-18 15:16:04 -0500

    mi primer cambio

---
+++ EjemploG2/src/williams202511799.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class williams202511799 {
+
+    public static void main(String[] arg){
+
+    ArrayList<String> Lista = new ArrayList<>();
+
+Lista.add("Rafael");
+Lista.add("David");
+Lista.add("Elias");
+
+
+        for (String v: Lista) {
+            System.out.println(v);
+
+        }
+    }
+        }
