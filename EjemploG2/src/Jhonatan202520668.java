6e4f5637bd8f4f4bf7527ccd606bcfb4b208bcdb
jhonatanaparicio82-Nick <jhonatan.aparicio82@gmail.com>
2026-03-18 15:17:42 -0500

    mi primer cambio

---
+++ EjemploG2/src/Jhonatan202520668.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Jhonatan202520668 {
+    public static void main(String[] args) {
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Jho");
+        lista.add("And");
+        lista.add("Art");
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+}
