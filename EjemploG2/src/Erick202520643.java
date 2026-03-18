de255ba87166346b95e9697bcedaa4ca1bd27c8a
ErickGonzalo <apazaerickcr7.com>
2026-03-18 15:17:39 -0500

    mi primer cambio

---
+++ EjemploG2/src/Erick202520643.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Erick202520643 {
+
+    public static void main(String[] arg) {
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Erick");
+        lista.add("Lionel");
+        lista.add("Dayana");
+        lista.add("Natalie");
+
+        for (String v:lista){
+            System.out.println(v);
+        }
+
+    }
+}
