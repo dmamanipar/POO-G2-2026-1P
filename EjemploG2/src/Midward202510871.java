2b5b68ba5a471a283538354c993107ebb090fdac
midwardcalsina-hash <midwardcalsina@gmail.com>
2026-03-18 15:18:07 -0500

    mi primer cambio

---
+++ EjemploG2/src/Midward202510871.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Midward202510871 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> lista=new ArrayList();
+        lista.add("Midward");
+        lista.add("David");
+        lista.add("Elias");
+
+        for (String v:lista) {
+            System.out.println(v);
+        }
+    }
+}
