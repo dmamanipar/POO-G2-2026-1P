b1223ceb70544665dc91456b8a0f0d5c7ad9fd7c
dmamanipar <dmamanipar@unsa.edu.pe>
2026-03-18 15:08:29 -0500

    mi primer cambio

---
+++ EjemploG2/src/David200410564.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class David200410564 {
+
+    public static void main(String[] arg){
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Rafael");
+        lista.add("David");
+        lista.add("Elias");
+
+        for (String v:lista) {
+            System.out.println(v);
+        }
+        
+    }
+
+}
