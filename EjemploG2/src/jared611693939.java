d0767ac153b9c11771915c9acacf8a18f1590785
Wireex61 <marceloflores6116@gmail.com>
2026-03-18 15:16:41 -0500

    mi primera chamba

---
+++ EjemploG2/src/jared611693939.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+
+
+
+public class jared611693939 {
+    public static void main(String[] args){
+
+
+   ArrayList<String> lista=new ArrayList<>();
+
+   lista.add("maria");
+   lista.add("marina");
+   lista.add("marcos");
+    for (String v:lista){
+        System.out.println(v);
+
+       }
+
+     }
+}
