7c9a0715e9c6315926a0f7a2c39bef3676d58807
HAD4D <hadkoredead@gmail.com>
2026-03-18 15:17:50 -0500

    mi primer cambio

---
+++ EjemploG2/src/Hadad202520181.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Hadad202520181 {
+    public static void main(String[] arg){
+
+        ArrayList<String> lista=new ArrayList<>();
+        lista.add("Henry");
+        lista.add("Elisabet");
+        lista.add("Heschler");
+
+        for (String v:lista){
+            System.out.println(v);
+        }
+    }
+}
