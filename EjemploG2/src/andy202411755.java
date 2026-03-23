1e741de2088a7465a5e238c6e2ddc097921b325c
Andy <ccazaatamariandyleonel@gmail.com>
2026-03-23 05:57:41 -0500

    mi primera clase

---
+++ EjemploG2/src/andy202411755.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class andy202411755 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("Rafael");
+        lista.add("David");
+        lista.add("Elias");
+
+        for (String v : lista) {
+            System.out.println(v);
+        }
+    }
+}
